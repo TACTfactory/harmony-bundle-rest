71a1443bc5fe6e2fb3324b456e9ef21586ca7e94
Erwan Le Huitouze <erwan.lehuitouze@tactfactory.com>
2013-08-22 16:19:07 +0200

    Update build.xml Copy rest-bundle.jar to platform lib at generation ref #1445

--- src/com/tactfactory/harmony/bundles/rest/template/RestGenerator.java
+++ src/com/tactfactory/harmony/bundles/rest/template/RestGenerator.java
@@ -53,6 +53,9 @@ public class RestGenerator extends BaseGenerator {
 	 * Generates everything.
 	 */
 	public final void generateAll() {
+		// Import Bundle for annotation
+		this.updateLibrary("rest-bundle.jar");
+				
 		this.generateWSAdapter();
 		try {
 			new TestWSGenerator(this.getAdapter()).generateAll();
