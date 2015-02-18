db485716ca909ff2aa588875e5ac90100d5a0300
Nicolas Rauflet <nicolas.rauflet@tactfactory.com>
2015-02-18 14:41:31 +0100

    Update #5102

--- src/com/tactfactory/harmony/bundles/rest/platform/ios/RestAdapterIos.java
+++ src/com/tactfactory/harmony/bundles/rest/platform/ios/RestAdapterIos.java
@@ -8,12 +8,14 @@
  */
 package com.tactfactory.harmony.bundles.rest.platform.ios;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.tactfactory.harmony.bundles.rest.platform.RestAdapter;
 import com.tactfactory.harmony.meta.EntityMetadata;
 import com.tactfactory.harmony.platform.ios.IosAdapter;
 import com.tactfactory.harmony.updater.IUpdater;
+import com.tactfactory.harmony.updater.impl.SourceFile;
 
 /**
  * RestAdapter Adapter for IOS.
@@ -24,8 +26,45 @@ public class RestAdapterIos extends IosAdapter implements RestAdapter {
 
     @Override
     public List<IUpdater> getRestUpdaters() {
-        // TODO Auto-generated method stub
-        return null;
+        List<IUpdater> result = new ArrayList<IUpdater>();
+
+        String templatePath = this.getTemplateSourceProviderPath();
+        String filePath = this.getSourcePath()
+                + "/Data/WebService/";
+ 
+        result.add(new SourceFile(
+                templatePath + "base/WebServiceClientAdapterBase.h",
+                filePath + "base/WebServiceClientAdapterBase.h",
+                true));
+
+        result.add(new SourceFile(
+                templatePath + "base/WebServiceClientAdapterBase.m",
+                filePath + "base/WebServiceClientAdapterBase.m",
+                true));
+
+        // Make Abstract Adapter Base general for all entities
+        result.add(new SourceFile(
+                templatePath + "WebServiceClientAdapter.h", 
+                filePath + "WebServiceClientAdapter.h",
+                false));
+        
+        result.add(new SourceFile(
+                templatePath + "WebServiceClientAdapter.m", 
+                filePath + "WebServiceClientAdapter.m",
+                false));
+
+        // Make RestClient
+        result.add(new SourceFile(
+                templatePath + "RPCClient.h", 
+                filePath + "RPCClient.h",
+                false));
+
+        result.add(new SourceFile(
+                templatePath + "RPCClient.m", 
+                filePath + "RPCClient.m",
+                false));
+
+        return result;
     }
 
     @Override
---
+++ tpl/ios/src/provider/TemplateWebServiceClientAdapter.m.ftl
@@ -0,0 +1,11 @@
+<@header?interpret />
+<#assign curr = entities[current_entity] />
+
+#import "${curr.name}WebServiceClientAdapter.h"
+
+/**
+ * RPC class for {@link ${curr.name}} WebServiceClient adapters.
+ */
+@implementation CommentWebServiceClientAdapter
+
+@end
